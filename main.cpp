c3646dbf5c2413859a4ee2858d5bbf2b2623bbf7
karasb1 <bohdankaras456@gmail.com>
2024-06-22 16:43:24 +0300

    The ex1 was done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void tab(char* txt)
+{
+	for (int i = 0; txt[i] != '\0'; i++)
+	{
+		if (txt[i] == ' ')
+		{
+			txt[i] = '	';
+		}
+	}
+}
+
+
+
+
 int main()
 {
-
+	char str[] = "Lorem ipsum dolor sit amet. Ut nisi sapiente et consequatur commodi ut iste perspiciatis? Et quam animi et enim quasi et incidunt dolores aut quod mollitia.";
+	tab(str);
+	cout << str << endl;
 	return 0;
 }
