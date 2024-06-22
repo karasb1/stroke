73157a9b0dcb2fb21a95906c8cafa7871a83c77c
karasb1 <bohdankaras456@gmail.com>
2024-06-22 17:00:35 +0300

    The ex2 was done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 using namespace std;
 
+void c(char* txt, int a = 0, int b = 0, int c = 0)
+{
+	for (int i = 0; txt[i] != '\0'; i++)
+	{
+		if (int(txt[i]) >= 65 && int(txt[i]) <= 90 || int(txt[i]) >= 97 && int(txt[i]) <= 122)
+		{
+			a++;
+		}
+		else if (int(txt[i]) >= 48 && int(txt[i]) <= 57)
+		{
+			b++;
+		}
+		else
+		{
+			c++;
+		}
+	}
+	cout << "Letters : " << a << endl;
+	cout << "Numbers : " << b << endl;
+	cout << "Other : " << c << endl;
+}
+
+
+
+
 int main()
 {
-
+	char str[] = "1234567890Lorem-ipsum-dolor-sit&amet.^Ut:nisi;sapiente?et consequatur commodi ut iste perspiciatis? Et quam animi et enim quasi et incidunt dolores aut quod mollitia.";
+	c(str);
 	return 0;
 }
