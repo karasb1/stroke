e0ce5847b19530d930487cf5fb4041c1385a8378
karasb1 <bohdankaras456@gmail.com>
2024-06-22 17:09:17 +0300

    The ex3 was done.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
+void w(char* txt, int words = 0)
+{
+	for (int i = 0; txt[i+1] != '\0'; i++)
+	{
+		if (txt[i+1] == ' ' || txt[i + 1] == '.' || txt[i + 1] == ',' || txt[i + 1] == '!' || txt[i + 1] == '?')
+		{
+			words++;
+		}
+	}
+	cout << "Words : " << words << endl;
+}
+
+
+
+
 int main()
 {
-
+	char str[] = "Lorem ipsum dolor sit amet.Ut nisi sapiente et consequatur commodi ut iste perspiciatis?Et quam animi et enim quasi et incidunt dolores aut quod mollitia.";
+	cout << str << endl;
+	w(str);
 	return 0;
 }
